4dc7afab6e767f6b846a478382a01856065ba12a
xummer <willx@gobeta.com.cn>
2014-03-14 18:08:15 +0800

    add modules

---
+++ mymodule.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*
+# Filename : mymodule.py
+
+def sayhi():
+  print 'Hey man, this is mymodule speaking.'
+
+version = '0.1'
+
+# End of mymodule.py
--- mysql_connect.py
+++ mysql_connect.py
@@ -3,3 +3,22 @@
 # Filename : mysql_connect.py
 
 import MySQLdb
+
+#SQL host
+
+# 打开数据库连接
+db = MySQLdb.connet("localhost","appadmin","admin","cptn");
+
+# 使用cursor()获取操作游标
+cursor = db.cursor();
+
+# 使用execute 执行SQL语句
+cursor.execute("SELECT VERSION()")
+
+# 使用fetchone()获取一条数据
+data = cursor.fetchone()
+
+print "Database version : %s " % data
+
+#关闭数据库连接
+db.close()
