076015591cff907fc8fc1de5a363772311a53cef
Xummer <xummer26@gmail.com>
2014-04-01 22:08:00 +0800

    git add raw_input.py

--- using_sys.py
+++ using_sys.py
@@ -4,6 +4,20 @@
 
 import sys
 
+#print
+print 'name: ' + 'xummer'
+#print会依次打印每个字符串，遇到逗号“,”会输出一个空格
+print 'hoby: ' , 'game'
+print 'one argument %s' % '1' 
+print 'two argument %s and %d' % ('1', 1)
+
+#print用r'' 表示''中的字符不转译
+print r'\n 2333 \t\n'
+
+#print 用'''...'''来简化\\\n换行
+
+
+#sys
 print 'The commend line arguments are:'
 for i in sys.argv:
   print i
